0cfce1410710708d8275ed51c825dfb24687277a
humstarman <lueyoung7@gmail.com>
2018-07-26 13:47:08 +0800

    init

---
+++ restart-svc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 0 set env
+FILE=info.env
+source ./${FILE}
+# 1 restart svc
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - restart Kubernetes componenets ..."
+COMPONENTS="etcd"
+[[ "flannel" == "${CNI}" ]]  && COMPONENTS+=" flanneld"
+COMPONENTS+=" kube-apiserver kube-controller-manager kube-scheduler docker kubelet kube-proxy"
+MASTER_COMPONENTS="etcd kube-apiserver kube-controller-manager kube-scheduler"
+GROUP="${ANSIBLE_GROUP}"
+ansible $GROUP -m shell -a "systemctl daemon-reload"
+for COMPONENT in $COMPONENTS; do
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - restart $COMPONENT ..."
+  if echo $MASTER_COMPONENTS | grep $COMPONENT > /dev/null 2>&1; then
+    GROUP="master"
+  else
+    GROUP="${ANSIBLE_GROUP}"
+  fi
+  ansible $GROUP -m shell -a "systemctl restart $COMPONENT"
+done
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - Kubernetes componenets restarted."
+exit 0
